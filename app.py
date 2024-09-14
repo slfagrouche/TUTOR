58e9a30c35dd776883f8bd399ff4cc9a8e77c50c
Saad Ahmad Sabri <105510022+SaadASTheDev@users.noreply.github.com>
2024-09-14 19:05:58 -0400

    "Updated requirements.txt and vercel.json files"

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from lxangchain.prompts import PromptTemplate
 import librosa
 import requests
 from dotenv import load_dotenv
